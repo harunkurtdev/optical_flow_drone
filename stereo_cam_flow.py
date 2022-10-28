9c7d1d4dcb027c96c707d7c9fd4f9d147f43b35d
harunlakodla <harunlakodla@gmail.com>
2022-10-29 00:42:52 +0300

    changed file

--- stereo_cam_flow.py
+++ stereo_cam_flow.py
@@ -1,6 +1,30 @@
 import cv2
 
+left = cv2.VideoCapture(2)
+right = cv2.VideoCapture(2)
 
-if __name__ == "__main__":
-    
-    print("stereo cam flow")
+while(True):
+    if not (left.grab() and right.grab()):
+        print("No more frames")
+        break
+
+    try:
+        
+        _, leftFrame = left.retrieve()
+        _, rightFrame = right.retrieve()
+        
+        
+        cv2.imshow('left', leftFrame)
+        cv2.imshow('right', rightFrame)
+
+    except Exception:
+        import time
+        time.sleep(0.5)
+        
+        # pass
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+left.release()
+right.release()
+cv2.destroyAllWindows()
